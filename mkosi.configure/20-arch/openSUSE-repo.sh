77bcf5a50b34b7df02ab99904aabd1fef4eb2fc8
FireKahuna <849166+TheFireKahuna@users.noreply.github.com>
2025-03-20 00:09:03 +1000

    Attempt 3 for openSUSE packages

--- mkosi.configure/20-arch/openSUSE-repo.sh
+++
@@ -1,6 +0,0 @@
-key=$(curl -fsSL https://download.opensuse.org/repositories/system:systemd/Arch/$(uname -m)/system_systemd_Arch.key)
-fingerprint=$(gpg --quiet --with-colons --import-options show-only --import --fingerprint <<< "${key}" | awk -F: '$1 == "fpr" { print $10 }')
-
-pacman-key --init
-pacman-key --add - <<< "${key}"
-pacman-key --lsign-key "${fingerprint}"
